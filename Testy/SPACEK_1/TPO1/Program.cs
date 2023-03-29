f057fbf2a07bad719c41de0d25b246bb737ec3fd
Hexonator <standaspacek25@gmail.com>
2023-03-29 12:24:12 +0200

    added tests

---
+++ Testy/SPACEK_1/TPO1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPO1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int num1, num2, num3, num4;
+                Console.Write("Prvni cislo: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.Write("Druhe cislo: ");
+                num2 = int.Parse(Console.ReadLine());
+                Console.Write("Treti cislo: ");
+                num3 = int.Parse(Console.ReadLine());
+                Console.Write("Ctvrte cislo: ");
+                num4 = int.Parse(Console.ReadLine());
+
+                int rect1, rect2;
+                rect1 = num1 * num2;
+                rect2 = num3 * num4;
+                if (rect1 > rect2)
+                    Console.WriteLine("Obdélník se stranami " + num1 + "x" + num2 + "cm má větší obsah než obdélník se stranami " + num3 + "x" + num4 + "cm.");
+                else
+                    Console.WriteLine("Obdélník se stranami " + num3 + "x" + num4 + "cm má větší obsah než obdélník se stranami " + num1 + "x" + num2 + "cm.");
+                Console.WriteLine("---------------------------");
+            }
+        }
+    }
+}
---
+++ Testy/SPACEK_2/TP05/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int A, B, num, count;
+
+Console.Write("A = ");
+A = int.Parse(Console.ReadLine());
+Console.Write("B = ");
+B = int.Parse(Console.ReadLine());
+
+if (A > B)
+{
+    int AB;
+    AB = A;
+    A = B;
+    B = AB;
+}
+
+Console.WriteLine($"<{A},{B}>");
+
+count = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Cislo {i+1} = ");
+    num = int.Parse(Console.ReadLine());
+    if (A <= num && num <= B)
+    {
+        count++;
+    }
+
+}
+
+Console.WriteLine("---------------");
+Console.WriteLine($"Čísel z intervalu je {count}");
+Console.ReadKey();

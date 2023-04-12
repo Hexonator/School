170c59cd0d95ef17e8524a054bc4a12cc1fb30e1
Hexonator <standaspacek25@gmail.com>
2023-04-12 16:27:00 +0200

    editss

--- 44_hledani_modusu/Program.cs
+++ 44_hledani_modusu/Program.cs
@@ -10,6 +10,16 @@ namespace _44_hledani_modusu
     {
         static void Main(string[] args)
         {
+            int[] array = new int[10] {1, 2, 1, 8, 8, 1, 4, 5, 9, 8};
+            Dictionary<int, int> number_counts = new Dictionary<int, int>();
+
+            foreach (var number in array)
+            {
+                if ()
+                {
+
+                }
+            }
         }
     }
 }

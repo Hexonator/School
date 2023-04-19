46ba3c7f56ec9da8da218883baebd578ea2fbfe2
Hexonator <standaspacek25@gmail.com>
2023-04-19 13:45:47 +0200

    19.4.

--- 44_hledani_modusu/Program.cs
+++ 44_hledani_modusu/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace _44_hledani_modusu
 {
@@ -11,15 +12,38 @@ namespace _44_hledani_modusu
         static void Main(string[] args)
         {
             int[] array = new int[10] {1, 2, 1, 8, 8, 1, 4, 5, 9, 8};
-            Dictionary<int, int> number_counts = new Dictionary<int, int>();
+            Dictionary<int, int> number_counts = new Dictionary<int, int>(array.Length);
 
             foreach (var number in array)
             {
-                if ()
+                if (number_counts.ContainsKey(number))
                 {
-
+                    number_counts[number]++;
+                }
+                else
+                {
+                    number_counts[number] = 1;
                 }
             }
+
+            int max = int.MinValue;
+            List<int> most_frequent_numbers = new List<int>(array.Length);
+            foreach(var key in number_counts.Keys)
+            {
+                if (number_counts[key] >= max)
+                {
+                    most_frequent_numbers.Add(key);
+                    max = number_counts[key];
+                }
+            }
+
+            Console.Write($"nejfrekventovanejsi cisla se objevila {max}x a jsou to:");
+            foreach (var number in most_frequent_numbers)
+            {
+                Console.Write($" {number},");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ 45_poradi_v_roce/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45_poradi_v_roce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] month_lengths = new int[12] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            Console.WriteLine("napis datum ve formatu X.X.");
+            Console.WriteLine();
+            while (true)
+            {
+                Console.Write("Datum: ");
+                string date = Console.ReadLine();
+
+                if (date == "")
+                {
+                    break;
+                }
+
+                int day = 0;
+                int month = 0;
+                bool monthDone = false;
+                foreach (var character in date)
+                {
+                    if (character != '.' && !monthDone)
+                    {
+                        month = int.Parse(Convert.ToString(month) + character);
+                    }
+                    else if (!monthDone)
+                    {
+                        monthDone = true;
+                        continue;
+                    }
+
+                    if (character != '.' && monthDone)
+                    {
+                        day = int.Parse(Convert.ToString(day) + character);
+                    }
+                }
+
+                int day_number = 0;
+                for (int i = 0; i < month-1; i++)
+                {
+                    day_number += month_lengths[i];
+                }
+                day_number += day;
+
+                Console.WriteLine(day_number);
+            }
+        }
+    }
+}

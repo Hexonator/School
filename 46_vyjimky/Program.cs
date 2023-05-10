525464b20640f1c5e738b50d65ab46fbc35963ec
Hexonator <standaspacek25@gmail.com>
2023-05-10 13:43:32 +0200

    10.5. 23

---
+++ 46_vyjimky/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_vyjimky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                try
+                {
+                    Console.Write("Zadej prvni cislo: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Zadej druhe cislo: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    double c = a / b;
+                    Console.WriteLine(c);
+                }
+                catch (DivideByZeroException exception)
+                {
+                    //Console.WriteLine("Nulou se neda delit");
+
+                    Console.WriteLine(exception.Message);
+                }
+                catch (FormatException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    // I kdyz je sama o sobe, napise, co je za problem ale neda se rozlisit, co se stane u vyjimek
+                }
+                finally
+                {
+                    Console.WriteLine("konec");
+                    //provede se vzdy
+                }
+
+                Console.WriteLine("--------------------");
+            }
+        }
+    }
+}
---
+++ 47_soubory1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; //Nastroje pro praci se soubory
+
+namespace _47_soubory1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr1 = new StreamReader("text.txt", Encoding.UTF8);
+            string s1 = sr1.ReadToEnd();
+            Console.WriteLine(s1);
+            sr1.Close();
+
+            using (StreamReader sr2 = new StreamReader(@"..\..\text2.txt", Encoding.UTF8)) // @"" = raw string; print("\\n") = \n
+            {
+                string s2 = sr2.ReadToEnd();
+                Console.WriteLine(s2);
+            }
+
+            //Cteni souboru a pocitani radku
+
+            using (StreamReader sr3 = new StreamReader(@"..\..\text2.txt", Encoding.UTF8))
+            {
+                string line;
+                int linecount = 0;
+                while ((line = sr3.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    linecount++;
+                }
+                Console.WriteLine($"text2.txt ma {linecount} radku");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}

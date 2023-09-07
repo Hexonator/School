f025607969eb656ee0a84f91d87a35ff8978bfb4
Hexonator <standaspacek25@gmail.com>
2023-09-07 12:35:20 +0200

    continued work

--- 2023-24/ST/01_posloupnosti/Program.cs
+++ 2023-24/ST/01_posloupnosti/Program.cs
@@ -11,7 +11,43 @@ namespace _01_posloupnosti
     {
         static void Main(string[] args)
         {
+                using(StreamReader posloupnosti = new StreamReader(@"..\..\posloupnosti.txt", Encoding.UTF8))
+                {
+                    string line;
+                    List<int> posloupnost = new List<int>(5);
+                    while(true)
+                    {
+                        line = posloupnosti.ReadLine();
+                        if (line == null)
+                        {
+                            break;
+                        }
+                        // for(int i = 0; i < line.Length; i++)
+                        string number = "";
+                        int counter = 0;
+                        foreach (var character in line)
+                        {
+                            if (character != ' '){
+                                number += character;
+                            }
+                            else{
+                                posloupnost.Add(int.Parse(number));
+                            }
+                            counter++;
+                        }
 
+                        for (int i = 0; i <= posloupnost.Count; i++)
+                        {
+                            if (posloupnost[i+1] + posloupnost[i])
+                            {
+                                
+                            }
+                        }
+
+
+                        posloupnost = new List<int>(5);
+                    }
+                }
         }
     }
 }

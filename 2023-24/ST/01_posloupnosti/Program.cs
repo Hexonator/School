62e6d306720af450fc41c2537bdf9b42d17f56cd
Hexonator <standaspacek25@gmail.com>
2023-09-07 21:50:20 +0200

    code works but doesn't have all functions

--- 2023-24/ST/01_posloupnosti/Program.cs
+++ 2023-24/ST/01_posloupnosti/Program.cs
@@ -11,43 +11,74 @@ namespace _01_posloupnosti
     {
         static void Main(string[] args)
         {
-                using(StreamReader posloupnosti = new StreamReader(@"..\..\posloupnosti.txt", Encoding.UTF8))
+            using(StreamReader data = new StreamReader(@"..\..\posloupnosti.txt", Encoding.UTF8))
+            {
+                string line;
+                int sequence_counter = 1;
+                List<int> posloupnost = new List<int>(5);
+                while(true)
                 {
-                    string line;
-                    List<int> posloupnost = new List<int>(5);
-                    while(true)
+                    line = data.ReadLine();
+                    if (line == null)
                     {
-                        line = posloupnosti.ReadLine();
-                        if (line == null)
+                        Console.WriteLine("end");
+                        break;
+                    }
+                    // for(int i = 0; i < line.Length; i++)
+                    string number = "";
+                    int counter = 0;
+                    foreach (var character in line)
+                    {
+                        if (character != ' '){
+                            number += character;
+                        }
+                        else{
+                            posloupnost.Add(int.Parse(number));
+                            number = "";
+                        }
+                        counter++;
+                    }
+                    posloupnost.Add(int.Parse(number));
+
+                    if (posloupnost.Count < 3)
+                    {
+                        Console.WriteLine($"Sequence {sequence_counter} is not long enough");
+                        posloupnost = new List<int>(5);
+                        sequence_counter++;
+                        continue;
+                    }
+
+                    int lastnum = 0;
+                    bool breakHappened = false;
+                    foreach (var num in posloupnost)
+                    {
+                        if (num < lastnum)
                         {
+                            Console.WriteLine($"Sequence {sequence_counter} is invalid. Numbers aren't in order");
+                            breakHappened = true;
                             break;
                         }
-                        // for(int i = 0; i < line.Length; i++)
-                        string number = "";
-                        int counter = 0;
-                        foreach (var character in line)
-                        {
-                            if (character != ' '){
-                                number += character;
-                            }
-                            else{
-                                posloupnost.Add(int.Parse(number));
-                            }
-                            counter++;
-                        }
-
-                        for (int i = 0; i <= posloupnost.Count; i++)
-                        {
-                            if (posloupnost[i+1] + posloupnost[i])
-                            {
-                                
-                            }
-                        }
-
-
-                        posloupnost = new List<int>(5);
+                        lastnum = num;
                     }
+
+                    if (breakHappened) { continue; }
+
+
+                    for (int i = 0; i <= posloupnost.Count; i++)
+                    {
+                        if (true)
+                        {
+                                
+                        }
+                    }
+
+
+                    posloupnost = new List<int>(5);
+                    sequence_counter++;
                 }
+            }
+
+            Console.ReadKey();
         }
     }
 }

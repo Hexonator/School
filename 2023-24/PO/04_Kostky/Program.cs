40823503ae76f7e33632b0e2e1877d08e9637998
Hexonator <standaspacek25@gmail.com>
2023-10-09 17:36:22 +0200

    school

--- 2023-24/PO/04_Kostky/Program.cs
+++ 2023-24/PO/04_Kostky/Program.cs
@@ -1,61 +1,91 @@
-﻿namespace _04_Hody_Kostkou;
+﻿using System.Data;
 
-class Dice()
+namespace _04_Hody_Kostkou;
+
+class Dice
 {
-    int num_of_sides, dice_num;
+    int num_of_sides;
+    int dice_num;
+    int throw_amount;
     Random rand = new();
+    int[] throw_probabilities = new int[10];
     public Dice()
     {
-        while (true)
+        if (!(num_of_sides > 0 && dice_num > 0)) # Makes testing easier, skips user entry if values are assigned
         {
-            Console.WriteLine("Enter number of dice:");
-            if (int.TryParse(Console.ReadLine(), out int dice_out))
+            while (true)
             {
-                dice_num = dice_out;
-                break;
+                Console.WriteLine("Enter number of dice:");
+                if (int.TryParse(Console.ReadLine(), out int dice_out))
+                {
+                    dice_num = dice_out;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number, try again.");
+                }
             }
-            else
+            while (true)
             {
-                Console.WriteLine("Invalid number, try again.");
+                Console.WriteLine("Enter number of sides per dice:");
+                if (int.TryParse(Console.ReadLine(), out int sides_out))
+                {
+                    num_of_sides = sides_out;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number, try again.");
+                }
             }
         }
-        while (true)
-        {
-            Console.WriteLine("Enter number of sides per dice:");
-            if (int.TryParse(Console.ReadLine(), out int sides_out))
-            {
-                num_of_sides = sides_out;
-                break;
-            }
-            else
-            {
-                Console.WriteLine("Invalid number, try again.");
-            }
-        }
+        int[] throws = new int[num_of_sides * dice_num + 1];
+        throw_probabilities = throws;
     }
-
-    int[] throw_probabilities = new[num_of_sides * dice_num];
 
     public int ThrowDice()
     {
         int throw_sum = 0;
-        for (int i = 0; i <= dice_num; i++)
+        for (int i = 0; i < dice_num; i++)
         {
-            throw_sum += rand.Next(0, num_of_sides);
+            throw_sum += rand.Next(1, num_of_sides + 1);
         }
         return throw_sum;
     }
 
+    public void ThrowDiceNTimes(int dice_throws)
+    {
+        int index;
+        for (int i = 0; i < dice_throws; i++)
+        {
+            index = ThrowDice();
+            if (index > 18)
+            {
+                Console.WriteLine("what");
+            }
+            throw_probabilities[index] += 1;
+        }
+        throw_amount = dice_throws;
+        WriteProbabilities();
+    }
+
     public void WriteProbabilities()
     {
-        int index = 3;
-        foreach (var num in throw_probabilities)
+        int throw_sum = 0;
+        double percentage;
+        foreach (var count_of_throws in throw_probabilities)
         {
-            Console.WriteLine($"sum {3} fell {num} times, which occured in {}% of cases"); #TODO add percent counter
-            index++;
+            if (count_of_throws == 0)
+            {
+                throw_sum++;
+                continue;
+            }
+            percentage = (double)count_of_throws / throw_amount * 100;
+            Console.WriteLine($"sum {throw_sum,-4} fell {count_of_throws,-8} times, which occurred in {Math.Round(percentage, 3),-8}% of cases");
+            throw_sum++;
         }
     }
-    #TODO make function that throws dice n times
 }
 
 class Program
@@ -63,6 +93,7 @@ class Program
     static void Main(string[] args)
     {
         Dice D6 = new();
-        
+        D6.ThrowDiceNTimes(1000000);
+        Console.ReadKey();
     }
 }

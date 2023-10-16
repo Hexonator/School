831389e152ab54ec63e83568c1fc741653eb630d
Hexonator <standaspacek25@gmail.com>
2023-10-16 14:19:23 +0200

    starting work
    I made a separate file with the class to test how it works by itself to copy it into the project itself later

---
+++ 2023-24/ST/12_graphDrawer/EvaluateExpression class/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace EvaluateExpression_class;
+
+class Program
+{
+    class EvaluateExpression
+    {
+        public int Evaluate(string expression)
+        {
+            
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        EvaluateExpression eval;
+        Console.WriteLine(eval.Evaluate("sin(cos(x))"));
+    }
+}

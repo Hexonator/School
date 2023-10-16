c4c34abdb737148979f947b40a78b5bf2e7858b4
Hexonator <standaspacek25@gmail.com>
2023-10-16 13:30:10 +0200

    wants me to merge. wish me luck. merge broke shit, everything is fixed

--- 2023-24/ST/12_graphDrawer/Form1.cs
+++ 2023-24/ST/12_graphDrawer/Form1.cs
@@ -1,37 +1,42 @@
-<<<<<<< HEAD
-using System.Runtime.CompilerServices;
-=======
-using MathNet.Numerics;
->>>>>>> aa7a2248f11201838eed29c5ba4445b1ae3d5749
+using System.Drawing;
 
 namespace _12_graphDrawer
 {
     public partial class Form1 : Form
     {
-        int height, width;
-        int increment = 10;
+        static int increment = 10;
+        static int step = 1;
+        public List<Point> graph_points = new(400);
+        public Point[] graph_points_to_draw;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        public int Evaluate(string input)
+        public Point[] Evaluate(string input)
         {
-            // x=0, sin=1, cos=2, tg=3, cotg=4
-            // +=10, -=20, *=30, /=40
-            try
+            return new Point[10]; // arbitrary wrong value
+        }
+        public bool GetGraphPoints(string input)
+        {
+            int width = graphField.Width;
+            int height = graphField.Height;
+            Evaluate(input);
+            for (int x = -width / 2; x < width / 2; x += step) // I'm scared that moving by step could cause it to not allign in the middle
             {
-                double result = Math(input);
+                double y = Math.Sin((double)x / 80) * 140;
+                graph_points.Add(new Point(x, (int)y));
             }
-            catch (Exception e)
-            {
-                infoBox.Text = $"Error: {e.Message}";
-                return int.MaxValue;
-            }
+            graph_points_to_draw = G_CoordsToF_Coords(graph_points.ToArray());
+            return true;
         }
 
-        public void DrawGrid(Graphics g, Pen G_pen, int width, int height, int unitline_len, int div_unitline_len_add)
+        public void DrawGrid(Graphics g, Pen G_pen, int unitline_len, int div_unitline_len_add)
         {
+            int width = graphField.Width;
+            int height = graphField.Height;
+
             // vertical
             g.DrawLine(G_pen, width / 2, 0, width / 2, height);
 
@@ -43,10 +48,10 @@ namespace _12_graphDrawer
                     unitline_len += div_unitline_len_add;
                     usedDivider = true;
                 }
-                g.DrawLine(G_pen, G_CoordsToF_Coords(width, height, -unitline_len / 2, i * increment),
-                    G_CoordsToF_Coords(width, height, unitline_len / 2, i * increment));
-                g.DrawLine(G_pen, G_CoordsToF_Coords(width, height, -unitline_len / 2, -i * increment),
-                    G_CoordsToF_Coords(width, height, unitline_len / 2, -i * increment));
+                g.DrawLine(G_pen, G_CoordsToF_Coords(-unitline_len / 2, i * increment),
+                    G_CoordsToF_Coords(unitline_len / 2, i * increment));
+                g.DrawLine(G_pen, G_CoordsToF_Coords(-unitline_len / 2, -i * increment),
+                    G_CoordsToF_Coords(unitline_len / 2, -i * increment));
                 if (usedDivider)
                 {
                     unitline_len -= div_unitline_len_add;
@@ -65,10 +70,10 @@ namespace _12_graphDrawer
                     unitline_len += div_unitline_len_add;
                     usedDivider = true;
                 }
-                g.DrawLine(G_pen, G_CoordsToF_Coords(width, height, i * increment, -unitline_len / 2),
-                    G_CoordsToF_Coords(width, height, i * increment, unitline_len / 2));
-                g.DrawLine(G_pen, G_CoordsToF_Coords(width, height, -i * increment, -unitline_len / 2),
-                    G_CoordsToF_Coords(width, height, -i * increment, unitline_len / 2));
+                g.DrawLine(G_pen, G_CoordsToF_Coords(i * increment, -unitline_len / 2),
+                    G_CoordsToF_Coords(i * increment, unitline_len / 2));
+                g.DrawLine(G_pen, G_CoordsToF_Coords(-i * increment, -unitline_len / 2),
+                    G_CoordsToF_Coords(-i * increment, unitline_len / 2));
                 if (usedDivider)
                 {
                     unitline_len -= div_unitline_len_add;
@@ -78,35 +83,59 @@ namespace _12_graphDrawer
 
         }
 
-        private void graphField_Paint(object sender, PaintEventArgs e)
+        public void graphField_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
             Pen G_pen = new(Color.Black, 1);
-            height = graphField.Height;
-            width = graphField.Width;
             int unitline_len = 10;
             int div_unitline_len_add = 6;
 
-            DrawGrid(g, G_pen, width, height, unitline_len, div_unitline_len_add);
-        }
-        static Point G_CoordsToF_Coords(int width, int height, int x, int y)
-        {
-            int newX = x + width / 2;
-            int newY = y + height / 2;
-            return new Point(newX, newY);
-        }
+            DrawGrid(g, G_pen, unitline_len, div_unitline_len_add);
 
-        private void textBox1_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
+            if (graph_points_to_draw != null && graph_points_to_draw.Length > 0)
             {
-                int text_result = Evaluate(EntryBox.Text);
-                if (text_result != int.MaxValue)
-                {
-                    
-                }
+                g.DrawLines(G_pen, graph_points_to_draw.ToArray());
             }
         }
 
+        public Point G_CoordsToF_Coords(int x, int y)
+        {
+            int width = graphField.Width;
+            int height = graphField.Height;
+            int newX = x + width / 2;
+            int newY = height / 2 - y;
+            return new Point(newX, newY);
+        }
+        public Point G_CoordsToF_Coords(Point point)
+        {
+            int width = graphField.Width;
+            int height = graphField.Height;
+            int newX = point.X + width / 2;
+            int newY = height / 2 - point.Y;
+            return new Point(newX, newY);
+        }
+        public Point[] G_CoordsToF_Coords(Point[] points)
+        {
+            Point[] newpoints = new Point[points.Length];
+            int width = graphField.Width;
+            int height = graphField.Height;
+            for (int i = 0; i < points.Length; i++)
+            {
+                newpoints[i] = new Point(points[i].X + width / 2, height / 2 - points[i].Y);
+            }
+            return newpoints;
+        }
+
+        private void EntryBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                bool pointSearchSuccess = GetGraphPoints(EntryBox.Text);
+                if (pointSearchSuccess)
+                {
+                    graphField.Refresh();
+                }
+            }
+        }
     }
 }

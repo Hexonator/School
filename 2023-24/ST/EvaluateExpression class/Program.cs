16d10b677b2cb3fcf16d05c9462e72c7523c1d53
Hexonator <standaspacek25@gmail.com>
2023-10-16 14:42:47 +0200

    moved folder due to forms app not working

---
+++ 2023-24/ST/EvaluateExpression class/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EvaluateExpression_class;
+
+class Program
+{
+    class EvaluateExpression
+    {
+        public double Evaluate(string expression)
+        {
+
+            return 1;
+        } 
+
+// TODO: implementovat infixový zápis např. +(x;2)
+        private List<string> CreateList()
+        {
+
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        EvaluateExpression eval = new();
+        Console.WriteLine(eval.Evaluate("sin(cos(x))"));
+    }
+}

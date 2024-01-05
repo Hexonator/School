64f01070527133e6507e54c7474d1b270df19d38
Hexonator <standaspacek25@gmail.com>
2024-01-05 11:07:28 +0100

    school

---
+++ 2023-24/ST/M01_prepis_VD/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace M01_prepis_VD;
+
+class Program
+{
+    static float GetAvgInInterval(int[] array, int bottom, int top){
+        float average = 0;
+        int count = 0;
+        foreach (int num in array)
+        {
+            if (num >= bottom && num <= top)
+            {
+                average += num;
+                count++;
+            }
+        }
+        if (count > 0){
+            return average/count;
+        } else {
+            return -1;
+        }
+
+    }
+    static void Main(string[] args)
+    {
+        int[] test_array = new int[10] {213, 87, 54, 0, 5, 70, -5, 437, 99, 1};
+        float average = GetAvgInInterval(test_array, 0, 100);
+        Console.WriteLine(average);
+
+        Console.ReadKey();
+    }
+}
